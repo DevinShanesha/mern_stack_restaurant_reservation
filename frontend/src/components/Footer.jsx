8e052c1b38c8f55cce4d45147bcc5f43c64cf671
Devin Shanesha <devin.shanesha@gmail.com>
2024-12-01 15:50:50 +0530

    mern stack restaurant reservation project

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">YOUR FOOD</div>
+          <div className="right">
+            <p>23D Shastri Nagar, Jodhpur</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Devin Shanesha</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By devinshanesha.com</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
